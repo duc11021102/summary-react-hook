430699fee35eba709a68ad8ff9c5f3412aec4be1
Duc <kieungocanhduc@gmail.com>
2023-08-01 18:17:35 +0700

    alternative useState by using useReducer

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,69 +1,101 @@
-import React, { useState, useCallback } from "react";
+import React, {  useCallback, useReducer } from "react";
 
 import IngredientForm from "./IngredientForm";
 import Search from "./Search";
 import IngredientList from "./IngredientList";
 import ErrorModal from "../UI/ErrorModal";
-function Ingredients() {
-  const [ingredients, setIngredients] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState();
 
+const initialIngredientslState = [];
+const initialHttpState = { loading: false, error: null };
+const ingredientsReducer = (currentIngredients, action) => {
+  switch (action.type) {
+    case 'SET':
+      return action.ingredients
+    case 'ADD':
+      return [...currentIngredients, action.ingredient]
+    case 'DELETE':
+      return currentIngredients.filter((ing) => ing.id !== action.id)
+    default:
+      throw new Error('Should not get there')
+  }
+}
+const httpReducer = (currHttpState, action) => {
+  switch (action.type) {
+    case 'SEND':
+      return { loading: true, error: null }
+    case 'RESPONSE':
+      return { ...currHttpState, loading: false }
+    case 'ERROR':
+      return { loading: false, error: action.errorMessage }
+    case 'CLEAR':
+      return { ...currHttpState, error: null }
+    default:
+      throw new Error('Should not get there')
+  }
+}
+
+const Ingredients = () => {
+  // const [isLoading, setIsLoading] = useState(false);
+  // const [error, setError] = useState();
+  const [userIngredients, dispatch] = useReducer(ingredientsReducer, initialIngredientslState);
+  const [httpState, dispatchHttp] = useReducer(httpReducer, initialHttpState);
 
   // useCallback giúp tạo lại hàm chỉ khi cần thiết và giữ nó không thay đổi giữa các lần re-render.
   // mỗi lần component thay đổi thì sẽ tạo lại hàm và chạy lại hàm từ đầu điều này sẽ không cần thiết nên sẽ sử dụng useCallback
   const filteredIngredientsHandler = useCallback(filteredIngredients => {
-    setIngredients(filteredIngredients);
+    dispatch({ type: 'SET', ingredients: filteredIngredients })
   }, []);
 
   const onAddIngredientHandler = (ingredient) => {
-    setIsLoading(true);
+    // setIsLoading(true);
+    dispatchHttp({ type: 'SEND' })
     fetch(
       "https://react-hook-80098-default-rtdb.firebaseio.com/ingredients.json",
       {
         method: "POST",
-        // mode: "no-cors",
         body: JSON.stringify(ingredient),
         headers: { "Content-Type": "application/json" },
       }
     )
       .then((response) => {
-        setIsLoading(false);
+        // setIsLoading(false);
+        dispatchHttp({ type: 'RESPONSE' })
+
         return response.json();
       })
       .then((responseData) => {
-        setIngredients((prev) => [
-          ...prev,
-          { id: responseData.name, ...ingredient },
-        ]);
+        dispatch({ type: 'ADD', ingredient: { id: responseData.name, ...ingredient } })
       });
   };
   const onRemoveItemHandler = (id) => {
-    setIsLoading(true);
+    // setIsLoading(true);
+    dispatchHttp({ type: 'SEND' })
     fetch(`https://react-hook-80098-default-rtdb.firebaseio.com/ingredients/${id}.json`, {
       method: 'DELETE'
     }).then((response) => {
-      setIsLoading(false);
-      const newIngredients = ingredients.filter((item) => item.id !== id);
-      setIngredients(newIngredients);
+      // setIsLoading(false);
+      dispatchHttp({ type: 'RESPONSE' })
+      dispatch({ type: 'DELETE', id: id })
     }).catch((error) => {
-      setError('Something went wrong');
+      // setError('Something went wrong');
+      dispatchHttp({ type: 'ERROR', errorMessage: "Something went wrong" })
+
     })
   };
-
   const onCloseHandler = () => {
-    setError(null);
-    setIsLoading(false);
+    // setError(null);
+    // setIsLoading(false);
+    dispatchHttp({type: 'CLEAR'})
   }
   return (
     <div className="App">
-      {error && <ErrorModal onClose={onCloseHandler}>{error}</ErrorModal>}
-      <IngredientForm onAddIngredient={onAddIngredientHandler} isLoading={isLoading} />
+      {httpState.error && <ErrorModal onClose={onCloseHandler}>{httpState.error}</ErrorModal>}
+      <IngredientForm onAddIngredient={onAddIngredientHandler} isLoading={httpState.loading} />
       <section>
         <Search onLoadIngredients={filteredIngredientsHandler} />
         <IngredientList
           onRemoveItem={onRemoveItemHandler}
-          ingredients={ingredients}
+          ingredients={userIngredients}
         />
       </section>
     </div>
